ea69123529a1c9cff35dc7b2b77b2bfd5da26c5e
Student <emilywright212@gmail.com>
2020-04-27 19:20:06 +0100

    method for formatting puzzles exists

--- spec/puzzle_display_spec.rb
+++ spec/puzzle_display_spec.rb
@@ -1,7 +1,9 @@
-class Puzzle
+require 'SodokuPuzzle'
 
-  def formatted_display(puzzle)
-"
+describe 'shows formatted puzzle' do
+  it 'shows formatted complete puzzle' do
+    puzzle = Puzzle.new
+    expect(puzzle.formatted_display(123456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq("
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
@@ -13,11 +15,7 @@ class Puzzle
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
-"
-  end
-
-  def verify_solution(solved_puzzle)
-    true
+")
   end
 
 end
