6c3ff57d92b51f3146d877ae3e9d5540d4948b7e
Student <emilywright212@gmail.com>
2020-04-27 20:01:22 +0100

    Refactor puzzle display row 1

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -1,10 +1,18 @@
 class Puzzle
 
   def formatted_display(puzzle_int)
+    output = "/n"
+    row = 1
+
     puzzle_array = puzzle_int.to_s.split(//)
-    first_three = "#{puzzle_array[0]}  #{puzzle_array[1]}  #{puzzle_array[2]}"
+    first_three = ""
+    for i in 0..1
+      first_three << puzzle_array[i] + "  "
+    end
+    first_three << puzzle_array[2] + " "
+
 "
-#{first_three} | #{puzzle_array[3]}  5  6 | 7  8  9
+#{first_three}| #{puzzle_array[3]}  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 --------------------------
