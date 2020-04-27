f54ab7ade34c27de0af48c6058a1ebf65154918e
Student <emilywright212@gmail.com>
2020-04-27 19:48:26 +0100

    formatted display can deal with first three numbers changing in puzzle

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -3,7 +3,7 @@ class Puzzle
   def formatted_display(puzzle_int)
     puzzle_array = puzzle_int.to_s.split(//)
 "
-#{puzzle_array[0]}  2  3 | 4  5  6 | 7  8  9
+#{puzzle_array[0]}  #{puzzle_array[1]}  #{puzzle_array[2]} | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 --------------------------
