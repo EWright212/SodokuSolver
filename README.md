04b90735f409c84ddcf80ba055864b6de98f362b
Student <emilywright212@gmail.com>
2020-04-27 19:36:18 +0100

    Initialised README

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO : rubocop and simplecov set up
+
+Steps
+ - can take input for puzzle
+ - can diplay puzzle in manner readable by humans
--- spec/puzzle_display_spec.rb
+++ spec/puzzle_display_spec.rb
@@ -1,7 +1,7 @@
 require 'SodokuPuzzle'
 
 describe 'shows formatted puzzle' do
-  it 'shows formatted complete puzzle' do
+  it 'shows formatted complete puzzle v1' do
     puzzle = Puzzle.new
     expect(puzzle.formatted_display(123456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq("
 1  2  3 | 4  5  6 | 7  8  9
@@ -17,5 +17,20 @@ describe 'shows formatted puzzle' do
 1  2  3 | 4  5  6 | 7  8  9
 ")
   end
-
+  it 'shows formatted complete puzzle v1' do
+    puzzle = Puzzle.new
+    expect(puzzle.formatted_display(123456781123456781123456781123456781123456781123456781123456781123456781123456781)).to eq("
+1  2  3 | 4  5  6 | 7  8  1
+1  2  3 | 4  5  6 | 7  8  1
+1  2  3 | 4  5  6 | 7  8  1
+--------------------------
+1  2  3 | 4  5  6 | 7  8  1
+1  2  3 | 4  5  6 | 7  8  1
+1  2  3 | 4  5  6 | 7  8  1
+--------------------------
+1  2  3 | 4  5  6 | 7  8  1
+1  2  3 | 4  5  6 | 7  8  1
+1  2  3 | 4  5  6 | 7  8  1
+")
+  end
 end
