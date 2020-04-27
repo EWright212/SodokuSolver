2f5303bfcd7016eafd12c4005077a1b1297d0afe
Student <emilywright212@gmail.com>
2020-04-27 20:26:01 +0100

    Refactor fixed rubocop warnings

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -4,21 +4,21 @@ class Puzzle
     puzzle_array = puzzle_int.to_s.split(//)
     formatted_puzzle = "\n"
     for i in 1..81
-      formatted_puzzle << puzzle_array[i-1]
+      formatted_puzzle << puzzle_array[i - 1]
       if i == 27 || i == 54
         formatted_puzzle << "\n--------------------------\n"
-      elsif i % 9 == 0
+      elsif (i % 9).zero?
         formatted_puzzle << "\n"
-      elsif i % 3 == 0
+      elsif (i % 3).zero?
         formatted_puzzle << " | "
       else
         formatted_puzzle << "  "
       end
     end
-  formatted_puzzle
+    formatted_puzzle
   end
 
-  def verify_solution(solved_puzzle)
+  def verify_solution(*)
     true
   end
 
