7797d66c68fe6bd90a1892aca3fc8de87f8a1d5d
Student <emilywright212@gmail.com>
2020-04-27 20:05:34 +0100

    Formatting for row 1 automated for first four digits

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -6,17 +6,17 @@ class Puzzle
 
     puzzle_array = puzzle_int.to_s.split(//)
     first_three = ""
-    for i in 1..3
+    for i in 1..4
       first_three << puzzle_array[i-1]
       if i % 3 == 0
-        first_three << " "
+        first_three << " | "
       else
         first_three << "  "
       end
     end
 
 "
-#{first_three}| #{puzzle_array[3]}  5  6 | 7  8  9
+#{first_three}5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
 --------------------------
