72f423c85f25b1f71aa6f64359e313b5a3238970
Student <emilywright212@gmail.com>
2020-04-28 11:46:28 +0100

    Column checker works

--- spec/column_checker_spec.rb
+++ spec/column_checker_spec.rb
@@ -1,12 +1,16 @@
 require 'sodoku_puzzle'
 
 describe 'column checker' do
-  it 'checks column 1 true' do
+  it 'checks all columns true' do
     puzzle = Puzzle.new
-    expect(puzzle.column_checker(199999999299999999399999999499999999599999999699999999799999999899999999999999999)).to eq true
+    expect(puzzle.column_checker(111111111222222222333333333444444444555555555666666666777777777888888888999999999)).to eq true
   end
   it 'checks column 1 false' do
     puzzle = Puzzle.new
     expect(puzzle.column_checker(199999999199999999399999999499999999599999999699999999799999999899999999999999999)).to eq false
+  end
+  it 'checks column 2 false' do
+    puzzle = Puzzle.new
+    expect(puzzle.column_checker(199999999299999999399999999499999999599999999699999999799999999899999999999999999)).to eq false
   end
 end
--- spec/row_checker_spec.rb
+++ spec/row_checker_spec.rb
@@ -1,7 +1,7 @@
 require 'sodoku_puzzle'
 
 describe 'row checker' do
-  it 'checks row 1 includes unique values true' do
+  it 'checks all rows true' do
     puzzle = Puzzle.new
     expect(puzzle.row_checker(123456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq true
   end
@@ -9,17 +9,9 @@ describe 'row checker' do
     puzzle = Puzzle.new
     expect(puzzle.row_checker(123416789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq false
   end
-  it 'checks row 2 includes unique values true' do
-    puzzle = Puzzle.new
-    expect(puzzle.row_checker(123456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq true
-  end
   it 'checks row 2 includes unique values false' do
     puzzle = Puzzle.new
     expect(puzzle.row_checker(123456789113456789123456789123456789123456789123456789123456789123456789123456789)).to eq false
-  end
-  it 'checks row 3 includes unique values true' do
-    puzzle = Puzzle.new
-    expect(puzzle.row_checker(123456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq true
   end
   it 'checks row 3 includes unique values false' do
     puzzle = Puzzle.new
