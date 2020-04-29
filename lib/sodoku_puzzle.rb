6227b4661265af271266864c0cef79065347681b
Student <emilywright212@gmail.com>
2020-04-29 16:18:03 +0100

    Row solver can fill in zeros with a nine

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -4,8 +4,17 @@ class Puzzle
     puzzle_int.to_s.length == 81 ? true : false
   end
 
-  def row_solver(*)
-    497258316186439725253716498629381547375964182841572639962145873718623954534897261
+  def row_solver(incomplete_puzzle)
+    puzzle_array = incomplete_puzzle.to_s.split(//)
+    solved_puzzle = puzzle_array.map do |digit|
+      digit = digit.to_i 
+      if digit == 0
+        digit = 9
+      else
+        digit
+      end
+    end
+    solved_puzzle.join.to_i
   end
 
 end
