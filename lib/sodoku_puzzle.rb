5db41f2e18e39c009ccaea7898e9471f9bd6aa2f
Student <emilywright212@gmail.com>
2020-04-29 11:23:08 +0100

    Refactor square sorter

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -1,7 +1,8 @@
 class Puzzle
   
+  # Square is defined as a 3 x 3 section of digits in a sodoku that must contain 1-9
   SQUARE_WIDTH = 3
-  ROW_WIDTH = 9
+  ROW_LENGTH = 9
 
   def check_solution_length(puzzle_int)
     puzzle_int.to_s.length == 81 ? true : false
@@ -111,15 +112,17 @@ class Puzzle
     puzzle_array = puzzle_int_to_array(filled_in_puzzle)
     current_square = []
     all_squares = []
-    # Squares are grouped in threes depending on which row they're in
-    for i in (0...ROW_WIDTH)
-      square_group = i / SQUARE_WIDTH
-      for k in (square_group * SQUARE_WIDTH...square_group * SQUARE_WIDTH + SQUARE_WIDTH)
-        for j in (ROW_WIDTH * k ... ROW_WIDTH * k + SQUARE_WIDTH)
-          current_square << puzzle_array[j + (SQUARE_WIDTH * (i - ( SQUARE_WIDTH * square_group)))]
+    # Squares are grouped in threes depending on which row they're in, e.g. Square 1 is in group 1, Square 4 is in group 2
+    for current_row in (0...ROW_LENGTH)
+      square_group = current_row / SQUARE_WIDTH
+      square_location = square_group * SQUARE_WIDTH
+      for current_square_row in (square_location...square_location + SQUARE_WIDTH)
+        current_digit = ROW_LENGTH * current_square_row
+        for j in (current_digit ... current_digit + SQUARE_WIDTH)
+          current_digit_index = j + SQUARE_WIDTH * (current_row - square_location)
+          current_square << puzzle_array[current_digit_index]
         end
       end
-      current_square.size == current_square.uniq.size ? solution_okay = true : solution_okay = false
       all_squares << current_square
       current_square = []
     end
