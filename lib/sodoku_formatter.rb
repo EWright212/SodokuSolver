e307dd27e1c143d963fd56d2cd0fce72996eb385
Student <emilywright212@gmail.com>
2020-04-29 15:21:04 +0100

    Refactor sodoku formatter to sep class

---
+++ lib/sodoku_formatter.rb
@@ -0,0 +1,22 @@
+class Formatter
+
+  def formatted_display(puzzle_int)
+    puzzle_array = puzzle_int.to_s.split(//)
+    formatted_puzzle = "\n"
+    for i in 1..81
+      formatted_puzzle << puzzle_array[i - 1]
+      
+      if i == 27 || i == 54
+        formatted_puzzle << "\n--------------------------\n"
+      elsif (i % 9).zero?
+        formatted_puzzle << "\n"
+      elsif (i % 3).zero?
+        formatted_puzzle << " | "
+      else
+        formatted_puzzle << "  "
+      end
+    end
+    formatted_puzzle
+  end
+
+end
