abe37c7cee61e64ce87dca5d130db28311b64ca4
Student <emilywright212@gmail.com>
2020-04-29 15:22:19 +0100

    Refactor formatter tests to reflect new class

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -12,25 +12,6 @@ class Puzzle
     puzzle_int.to_s.split(//)
   end
 
-  def formatted_display(puzzle_int)
-    puzzle_array = puzzle_int_to_array(puzzle_int)
-    formatted_puzzle = "\n"
-    for i in 1..81
-      formatted_puzzle << puzzle_array[i - 1]
-      
-      if i == 27 || i == 54
-        formatted_puzzle << "\n--------------------------\n"
-      elsif (i % 9).zero?
-        formatted_puzzle << "\n"
-      elsif (i % 3).zero?
-        formatted_puzzle << " | "
-      else
-        formatted_puzzle << "  "
-      end
-    end
-    formatted_puzzle
-  end
-
   def verify_solution(filled_in_puzzle)
     row = row_checker(filled_in_puzzle)
     column = column_checker(filled_in_puzzle)
--- spec/puzzle_display_spec.rb
+++ spec/puzzle_display_spec.rb
@@ -18,7 +18,7 @@ describe 'shows formatted formatted_puzzle' do
 ")
   end
   it 'shows formatted complete formatted_puzzle v2' do
-    formatted_puzzle = Puzzle.new
+    formatted_puzzle = Formatter.new
     expect(formatted_puzzle.formatted_display(223456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq("
 2  2  3 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
@@ -34,7 +34,7 @@ describe 'shows formatted formatted_puzzle' do
 ")
   end
   it 'shows formatted complete formatted_puzzle v3' do
-    formatted_puzzle = Puzzle.new
+    formatted_puzzle = Formatter.new
     expect(formatted_puzzle.formatted_display(331456789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq("
 3  3  1 | 4  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
@@ -50,7 +50,7 @@ describe 'shows formatted formatted_puzzle' do
 ")
   end
   it 'shows formatted complete formatted_puzzle v4' do
-    formatted_puzzle = Puzzle.new
+    formatted_puzzle = Formatter.new
     expect(formatted_puzzle.formatted_display(111156789123456789123456789123456789123456789123456789123456789123456789123456789)).to eq("
 1  1  1 | 1  5  6 | 7  8  9
 1  2  3 | 4  5  6 | 7  8  9
@@ -66,7 +66,7 @@ describe 'shows formatted formatted_puzzle' do
 ")
   end
   it 'shows formatted complete formatted_puzzle v5' do
-    formatted_puzzle = Puzzle.new
+    formatted_puzzle = Formatter.new
     expect(formatted_puzzle.formatted_display(123456781123456781123456781123456781123456781123456781123456781123456781123456781)).to eq("
 1  2  3 | 4  5  6 | 7  8  1
 1  2  3 | 4  5  6 | 7  8  1
