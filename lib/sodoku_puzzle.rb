18bd8be48c8412ced4b96183c41e0bb269613d79
Student <emilywright212@gmail.com>
2020-04-29 16:26:38 +0100

    Refactor split up row solver method

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -6,8 +6,8 @@ class Puzzle
 
   def row_solver(incomplete_puzzle)
     puzzle_array = incomplete_puzzle.to_s.split(//)
-    solved_puzzle = puzzle_array.map do |digit|
-      digit = digit.to_i 
+    puzzle_in_progress = puzzle_array.map { |digit| digit = digit.to_i }
+    solved_puzzle = puzzle_in_progress.map do |digit|
       if digit == 0
         digit = 9
       else
@@ -16,5 +16,13 @@ class Puzzle
     end
     solved_puzzle.join.to_i
   end
+
+  # def get_current_row
+  #   for i in (0...9)
+  #     current_row = puzzle_array[(9 * i)...(9 * (i + 1))]
+  #     current_row.size == current_row.uniq.size ? solution_okay = true : solution_okay = false
+  #     break if solution_okay == false
+  #   end
+  # end
 
 end
