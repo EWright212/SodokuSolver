ceefaa899725c12f1beefbe002a3a89219ac2f97
Student <emilywright212@gmail.com>
2020-04-30 19:51:40 +0100

    rename tests to be useful

--- spec/spike_digit_hash_spec.rb
+++ spec/spike_digit_hash_spec.rb
@@ -1,7 +1,7 @@
 require './lib/sodoku_creator.rb'
 
-describe 'testy' do
-  it 'idk' do
+describe 'Test output converting puzzle to digit objects' do
+  it 'can handle output of one digit' do
     puzzle = NewSodoku.new
     expect(puzzle.puzzle_digit_array_to_string(4)).to eq("[#<struct DigitObject digit=4, location=0, column=0, row=0, square=0>]")
   end
