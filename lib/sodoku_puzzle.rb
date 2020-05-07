c6f6134f6a7190c27a425c22c035d797c7d6ecc2
Student <emilywright212@gmail.com>
2020-05-07 10:41:58 +0100

    Refactor - linting

--- lib/sodoku_puzzle.rb
+++ lib/sodoku_puzzle.rb
@@ -13,32 +13,29 @@ class Puzzle
     puzzle = NewSodoku.new
     puzzle_digit_location_hash = puzzle.create_puzzle_hash(incomplete_puzzle)
     solved_puzzle = []
-    until puzzle_digit_location_hash.select {|location, properties| properties.digit == 0} == {}
-      complete_map = puzzle_digit_location_hash.each do |location, properties|
-        if properties.digit == 0
+    until puzzle_digit_location_hash.select { |location, properties| properties.digit == 0 } == {}
+      puzzle_digit_location_hash.each do |location, properties|
+        if properties.digit.zero?
           properties.possibilities = square_digit_solver(location, properties, puzzle_digit_location_hash)
-          if properties.digit == 0
+          if properties.digit.zero?
             properties.possibilities = row_digit_solver(location, properties, puzzle_digit_location_hash)
-            if properties.digit == 0
+            if properties.digit.zero?
               properties.possibilities = column_digit_solver(location, properties, puzzle_digit_location_hash)
             end
           end
         end
       end
     end
-    puzzle_digit_location_hash.map {|location, properties| solved_puzzle << properties.digit}
+    puzzle_digit_location_hash.map { |_location, properties| solved_puzzle << properties.digit }
     solved_puzzle.join.to_i
   end
 
   private
 
-  #  TODO - step one - get these solvers to remove invalid options
-  #  TODO - step two - to aviod infinite loop - complete solver goes through each solver for each number only once
-
-  def square_digit_solver(location, properties, puzzle_digit_location_hash)
+  def square_digit_solver(_location, properties, puzzle_digit_location_hash)
     square_int = properties.square
     all_items_in_square = square_selector(square_int, puzzle_digit_location_hash)
-    square_all_digits = all_items_in_square.map { |location, properties| properties.digit}
+    square_all_digits = all_items_in_square.map { |_location, properties| properties.digit }
     properties.possibilities = properties.possibilities - square_all_digits
     if properties.possibilities.length == 1
       properties.digit = properties.possibilities[0]
@@ -46,10 +43,10 @@ class Puzzle
     properties.possibilities
   end
 
-  def row_digit_solver(location, properties, puzzle_digit_location_hash)
+  def row_digit_solver(_location, properties, puzzle_digit_location_hash)
     row_int = properties.row
     all_items_in_row = row_selector(row_int, puzzle_digit_location_hash)
-    row_all_digits = all_items_in_row.map { |location, properties| properties.digit}
+    row_all_digits = all_items_in_row.map { |_location, properties| properties.digit }
     properties.possibilities = properties.possibilities - row_all_digits
     if properties.possibilities.length == 1
       properties.digit = properties.possibilities[0]
@@ -57,10 +54,10 @@ class Puzzle
     properties.possibilities
   end
 
-  def column_digit_solver(location, properties, puzzle_digit_location_hash)
+  def column_digit_solver(_location, properties, puzzle_digit_location_hash)
     column_int = properties.column
     all_items_in_column = column_selector(column_int, puzzle_digit_location_hash)
-    column_all_digits = all_items_in_column.map { |location, properties| properties.digit}
+    column_all_digits = all_items_in_column.map { |_location, properties| properties.digit }
     properties.possibilities = properties.possibilities - column_all_digits
     if properties.possibilities.length == 1
       properties.digit = properties.possibilities[0]
