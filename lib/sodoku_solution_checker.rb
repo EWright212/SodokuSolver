f925895e9a2c4f91f383707e56d4d1a9b181d2e2
Student <emilywright212@gmail.com>
2020-05-07 11:10:38 +0100

    Refactor - linting

--- lib/sodoku_solution_checker.rb
+++ lib/sodoku_solution_checker.rb
@@ -49,22 +49,22 @@ class SolutionChecker
 
   def digit_selector(section)
     digits_array = []
-    section.each {|location, properties| digits_array << properties.digit }
+    section.each { |_location, properties| digits_array << properties.digit }
     digits_array
   end
 
   # TODO - 3 methods below super similar - combine?
 
   def row_selector(row_int, puzzle_digit_location_hash)
-    puzzle_digit_location_hash.select {|location, properties| properties.row == row_int}
+    puzzle_digit_location_hash.select { |_location, properties| properties.row == row_int }
   end
 
   def column_selector(column_int, puzzle_digit_location_hash)
-    puzzle_digit_location_hash.select {|location, properties| properties.column == column_int}
+    puzzle_digit_location_hash.select { |_location, properties| properties.column == column_int }
   end
 
   def square_selector(square_int, puzzle_digit_location_hash)
-    puzzle_digit_location_hash.select {|location, properties| properties.square == square_int}
+    puzzle_digit_location_hash.select { |_location, properties| properties.square == square_int }
   end
 
 end
